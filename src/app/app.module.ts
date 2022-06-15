2bcc35aa02152aa017f2a95cd7fb919c6b6dbbef
Denis Souza <denis.souza@ulteriorti.com.br>
2022-06-15 15:18:27 -0300

    Finalizada modularização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
-import { Error404Component } from './error404/error404.component';
 import { HttpClientModule } from '@angular/common/http';
-import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    Error404Component,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    //CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       }, {
         path: 'courses', loadChildren: () => import('./courses/course.module').then(m => m.CourseModule)
       }, {
-        path: '**', component: Error404Component
+        path: '**', loadChildren: () => import('./core/core.module').then(m => m.CoreModule)
       }
     ])
   ],
---
+++ src/app/shared/component/star/star.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { StarComponent } from "./star.component";
+
+@NgModule({
+    imports: [StarComponent],
+    declarations: [
+
+    ],
+    exports: [
+        StarComponent
+    ]
+})
+export class StarModule {
+
+}
